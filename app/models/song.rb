023b6c0a0f4cd23bec18ca04449fa6f18e3630c3
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-18 22:40:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -18,4 +18,21 @@ class Song < ActiveRecord::Base
     self.try(:artist).try(:name)
   end
 
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip  !=''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
 end
