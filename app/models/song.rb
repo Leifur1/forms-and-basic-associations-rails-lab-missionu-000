b11373bb5d9549107a0904a49cfc269c4687e0cd
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-18 22:36:44 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,11 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   belongs_to :artist
   belongs_to :genre
+
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
 end
